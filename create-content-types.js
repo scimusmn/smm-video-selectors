aa02ed8168e8475f50955aa0ade4c3aae8c58810
Caly McMorrow <cmcmorrow@smm.org>
2022-04-08 14:32:54 -0500

    add scripts to create content model, entries, and upload assets, remove unused code

---
+++ create-content-types.js
@@ -0,0 +1,230 @@
+require('dotenv').config({
+  path: '.env.development',
+});
+
+// DOCS!
+// https://contentful.github.io/contentful-management.js/contentful-management/9.0.0/interfaces/ContentType.html#update
+/*
+  TODO:
+  Editor interface methods to add help text
+  Add script to package.json
+*/
+
+const contentful = require('contentful-management');
+
+// Use the contentful management api token, not the content delivery api
+const client = contentful.createClient({
+  accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+});
+
+const envName = process.env.CONTENTFUL_ENVIRONMENT;
+const spaceId = process.env.CONTENTFUL_SPACE_ID;
+
+// Create video with caption content type
+client.getSpace(spaceId)
+  .then((space) => space.getEnvironment(envName))
+  .then((environment) => environment.createContentTypeWithId('videoWithCaptionTest', {
+    name: 'Video with caption',
+    description: 'Pairs a video asset with one or more caption files',
+    fields: [
+      {
+        id: 'title',
+        name: 'Title',
+        type: 'Symbol',
+        localized: false,
+        required: false,
+        validations: [],
+        disabled: false,
+        omitted: false,
+      },
+      {
+        id: 'titleDisplay',
+        name: 'Title Display',
+        type: 'Symbol',
+        localized: false,
+        required: false,
+        validations: [],
+        disabled: false,
+        omitted: false,
+      },
+      {
+        id: 'Media',
+        name: 'Media',
+        type: 'Link',
+        localized: false,
+        required: false,
+        validations: [
+          {
+            linkMimetypeGroup: [
+              'image',
+              'video',
+            ],
+          },
+        ],
+        disabled: false,
+        omitted: false,
+        linkType: 'Asset',
+      },
+      {
+        id: 'caption',
+        name: 'caption',
+        type: 'Link',
+        localized: false,
+        required: false,
+        validations: [],
+        disabled: false,
+        omitted: false,
+        linkType: 'Asset',
+      },
+      {
+        id: 'selectionImage',
+        name: 'Selection Image',
+        type: 'Link',
+        localized: false,
+        required: false,
+        validations: [
+          {
+            linkMimetypeGroup: [
+              'image',
+            ],
+          },
+        ],
+        disabled: false,
+        omitted: false,
+        linkType: 'Asset',
+      },
+    ],
+  }))
+  .then((contentType) => contentType.publish())
+  .then((contentType) => {
+    console.log(`${contentType.sys.id} is published`);
+    // Create empty video with caption entry
+    client.getSpace(spaceId)
+      .then((space) => space.getEnvironment(envName))
+      .then((environment) => environment.createEntry('videoWithCaptionTest', {
+        fields: {
+          title: { 'en-US': 'Example Video with Caption' },
+          titleDisplay: { 'en-US': 'Awesome Example Video!' },
+        },
+      }))
+      .then((entry) => console.log(entry));
+  })
+  .catch(console.error);
+
+// Create video selector content type
+client.getSpace(spaceId)
+  .then((space) => space.getEnvironment(envName))
+  .then((environment) => environment.createContentTypeWithId('videoSelectorTest', {
+    name: 'Video Selector',
+    description: 'Creates a video selector kiosk app',
+    displayField: 'slug',
+    fields: [
+      {
+        id: 'slug',
+        name: 'Slug',
+        type: 'Symbol',
+        localized: false,
+        required: true,
+        validations: [],
+        disabled: false,
+        omitted: false,
+      },
+      {
+        id: 'title',
+        name: 'Title',
+        type: 'Symbol',
+        localized: false,
+        required: false,
+        validations: [],
+        disabled: false,
+        omitted: false,
+      },
+      {
+        id: 'displayTitle',
+        name: 'Display Title',
+        type: 'Symbol',
+        localized: true,
+        required: false,
+        validations: [],
+        disabled: false,
+        omitted: false,
+      },
+      {
+        id: 'backgroundGraphic',
+        name: 'Background graphic',
+        type: 'Link',
+        localized: false,
+        required: false,
+        validations: [
+          {
+            linkMimetypeGroup: [
+              'image',
+              'video',
+            ],
+          },
+        ],
+        disabled: false,
+        omitted: false,
+        linkType: 'Asset',
+      },
+      {
+        id: 'screenWidth',
+        name: 'Screen width',
+        type: 'Integer',
+        localized: false,
+        required: true,
+        validations: [],
+        disabled: false,
+        omitted: false,
+      },
+      {
+        id: 'screenHeight',
+        name: 'Screen height',
+        type: 'Integer',
+        localized: false,
+        required: true,
+        validations: [],
+        disabled: false,
+        omitted: false,
+      },
+      {
+        id: 'selections',
+        name: 'Selections',
+        type: 'Array',
+        localized: false,
+        required: true,
+        validations: [],
+        disabled: false,
+        omitted: false,
+        items: {
+          type: 'Link',
+          validations: [
+            {
+              linkContentType: [
+                'videoWithCaption',
+              ],
+            },
+          ],
+          linkType: 'Entry',
+        },
+      },
+    ],
+  }))
+  .then((contentType) => contentType.publish())
+  .then((contentType) => {
+    console.log(`${contentType.sys.id} is published`);
+    // Create empty entry
+    client.getSpace(spaceId)
+      .then((space) => space.getEnvironment(envName))
+      .then((environment) => environment.createEntry('videoSelectorTest', {
+        fields: {
+          slug: { 'en-US': 'example2' },
+          title: { 'en-US': 'My Awesome Example' },
+          displayTitle: { 'en-US': 'My Awesome Example' },
+          screenWidth: { 'en-US': 1920 },
+          screenHeight: { 'en-US': 1080 },
+        },
+      }))
+      .then((entry) => console.log(entry));
+  })
+  .catch(console.error);
