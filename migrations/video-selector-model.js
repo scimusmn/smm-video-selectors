dcf80632c32124f9a1d03ee73d6fcf3fe4256f57
Caly McMorrow <cmcmorrow@smm.org>
2022-04-28 14:59:59 -0500

    refactors migration scripts to use CLI, adds package and script to clear content models, content, and/or assets

--- migrations/video-selector-model.js
+++ migrations/video-selector-model.js
@@ -1,260 +1,207 @@
-/* eslint-disable no-param-reassign */
-require('dotenv').config({
-  path: './.env.development',
-});
+module.exports = function (migration) {
+  /* Video with caption */
 
-// DOCS!
-// https://contentful.github.io/contentful-management.js/contentful-management/9.0.0/interfaces/ContentType.html#update
-
-const contentful = require('contentful-management');
-
-// Use the contentful management api token, not the content delivery api
-const client = contentful.createClient({
-  accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
-});
-
-const envName = process.env.CONTENTFUL_ENVIRONMENT;
-const spaceId = process.env.CONTENTFUL_SPACE_ID;
-
-// Create video with caption content type
-client.getSpace(spaceId)
-  .then((space) => space.getEnvironment(envName))
-  .then((environment) => environment.createContentTypeWithId('videoWithCaption', {
+  const videoWithCaption = migration.createContentType('videoWithCaption', {
+    displayField: 'title',
     name: 'Video with caption',
-    description: 'Pairs a video asset with one or more caption files',
-    fields: [
+    description:
+    'Pairs a video asset with one or more caption files',
+  });
+
+  videoWithCaption.createField('title', {
+    name: 'Title',
+    type: 'Symbol',
+    localized: true,
+    required: false,
+    validations: [],
+    disabled: false,
+    omitted: false,
+  });
+
+  videoWithCaption.changeFieldControl('title', 'builtin', 'singleLine', {
+    helpText: 'Internal use only - not displayed in the app',
+  });
+
+  videoWithCaption.createField('titleDisplay', {
+    name: 'Title Display',
+    type: 'Symbol',
+    localized: true,
+    required: false,
+    validations: [],
+    disabled: false,
+    omitted: false,
+  });
+
+  videoWithCaption.changeFieldControl('titleDisplay', 'builtin', 'singleLine', {
+    helpText: 'Optional title for selection button',
+  });
+
+  videoWithCaption.createField('media', {
+    name: 'Media',
+    type: 'Link',
+    localized: false,
+    required: false,
+    validations: [
       {
-        id: 'title',
-        name: 'Title',
-        type: 'Symbol',
-        localized: false,
-        required: false,
-        validations: [],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'titleDisplay',
-        name: 'Title Display',
-        type: 'Symbol',
-        localized: false,
-        required: false,
-        validations: [],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'Media',
-        name: 'Media',
-        type: 'Link',
-        localized: false,
-        required: false,
-        validations: [
-          {
-            linkMimetypeGroup: [
-              'image',
-              'video',
-            ],
-          },
+        linkMimetypeGroup: [
+          'image',
+          'video',
         ],
-        disabled: false,
-        omitted: false,
-        linkType: 'Asset',
-      },
-      {
-        id: 'caption',
-        name: 'caption',
-        type: 'Link',
-        localized: false,
-        required: false,
-        validations: [],
-        disabled: false,
-        omitted: false,
-        linkType: 'Asset',
-      },
-      {
-        id: 'selectionImage',
-        name: 'Selection Image',
-        type: 'Link',
-        localized: false,
-        required: false,
-        validations: [
-          {
-            linkMimetypeGroup: [
-              'image',
-            ],
-          },
-        ],
-        disabled: false,
-        omitted: false,
-        linkType: 'Asset',
       },
     ],
-  }))
-  .then((contentType) => contentType.publish())
-  .then((contentType) => {
-    console.log(`${contentType.sys.id} is published`);
+    disabled: false,
+    omitted: false,
+    linkType: 'Asset',
+  });
 
-    // Add help text
-    client.getSpace(spaceId)
-      .then((space) => space.getEnvironment(envName))
-      .then((environment) => environment.getEditorInterfaceForContentType('videoWithCaption'))
-      .then((editorInterface) => {
-        editorInterface.controls[0].settings = { helpText: 'Internal title. This is not displayed in the application.' };
-        editorInterface.controls[1].settings = { helpText: 'Name of the video. This will populate the selection name on the home screen' };
-        editorInterface.controls[2].settings = { helpText: 'The video associated with the caption file' };
-        editorInterface.controls[3].settings = { helpText: 'Caption file. Only use files with .vtt extension.' };
-        editorInterface.controls[4].settings = { helpText: 'Image to accompany video selection on the home screen. Can be used as the selection "button".' };
-        return editorInterface.update();
-      })
-      .then((editorInterface) => console.log(`Editor interface ${editorInterface.sys.id} updated.`))
-      .catch(console.error);
-    // IN PROGRESS - Create empty video with caption entry
-    // client.getSpace(spaceId)
-    //   .then((space) => space.getEnvironment(envName))
-    //   .then((environment) => environment.createEntry('videoWithCaption', {
-    //     fields: {
-    //       title: { 'en-US': 'Example Video with Caption' },
-    //       titleDisplay: { 'en-US': 'Awesome Example Video!' },
-    //     },
-    //   }))
-    // .then((entry) => console.log(entry));
-  })
-  .catch(console.error);
+  videoWithCaption.changeFieldControl('media', 'builtin', 'assetLinkEditor', {
+    helpText: 'Video files must be .mp4 format',
+  });
 
-// Create video selector content type
-client.getSpace(spaceId)
-  .then((space) => space.getEnvironment(envName))
-  .then((environment) => environment.createContentTypeWithId('videoSelector', {
+  videoWithCaption.createField('caption', {
+    name: 'caption',
+    type: 'Link',
+    localized: false,
+    required: false,
+    validations: [],
+    disabled: false,
+    omitted: false,
+    linkType: 'Asset',
+  });
+
+  videoWithCaption.changeFieldControl('caption', 'builtin', 'assetLinkEditor', {
+    helpText: 'Must use files in .vtt format',
+  });
+
+  videoWithCaption.createField('selectionImage', {
+    name: 'Selection image',
+    type: 'Link',
+    localized: false,
+    required: false,
+    validations: [
+      {
+        linkMimetypeGroup: [
+          'image',
+        ],
+      },
+    ],
+    disabled: false,
+    omitted: false,
+    linkType: 'Asset',
+  });
+
+  /* Video Selector */
+
+  const videoSelector = migration.createContentType('videoSelector', {
+    displayField: 'title',
     name: 'Video Selector',
     description: 'Creates a video selector kiosk app',
-    displayField: 'slug',
-    fields: [
+  });
+
+  videoSelector.createField('slug', {
+    name: 'Slug',
+    type: 'Symbol',
+    localized: false,
+    required: false,
+    validations: [],
+    disabled: false,
+    omitted: false,
+  });
+
+  videoSelector.createField('title', {
+    name: 'Title',
+    type: 'Symbol',
+    localized: true,
+    required: false,
+    validations: [],
+    disabled: false,
+    omitted: false,
+  });
+
+  videoSelector.createField('titleDisplay', {
+    name: 'Title Display',
+    type: 'Symbol',
+    localized: true,
+    required: false,
+    validations: [],
+    disabled: false,
+    omitted: false,
+  });
+
+  videoSelector.changeFieldControl('titleDisplay', 'builtin', 'singleLine', {
+    helpText: 'Optional title for selection screen',
+  });
+
+  videoSelector.createField('backgroundGraphic', {
+    name: 'Media',
+    type: 'Link',
+    localized: false,
+    required: false,
+    validations: [
       {
-        id: 'slug',
-        name: 'Slug',
-        type: 'Symbol',
-        localized: false,
-        required: true,
-        validations: [],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'title',
-        name: 'Title',
-        type: 'Symbol',
-        localized: false,
-        required: false,
-        validations: [],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'displayTitle',
-        name: 'Display Title',
-        type: 'Symbol',
-        localized: true,
-        required: false,
-        validations: [],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'backgroundGraphic',
-        name: 'Background graphic',
-        type: 'Link',
-        localized: false,
-        required: false,
-        validations: [
-          {
-            linkMimetypeGroup: [
-              'image',
-              'video',
-            ],
-          },
+        linkMimetypeGroup: [
+          'image',
+          'video',
         ],
-        disabled: false,
-        omitted: false,
-        linkType: 'Asset',
-      },
-      {
-        id: 'screenWidth',
-        name: 'Screen width',
-        type: 'Integer',
-        localized: false,
-        required: true,
-        validations: [],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'screenHeight',
-        name: 'Screen height',
-        type: 'Integer',
-        localized: false,
-        required: true,
-        validations: [],
-        disabled: false,
-        omitted: false,
-      },
-      {
-        id: 'selections',
-        name: 'Selections',
-        type: 'Array',
-        localized: false,
-        required: true,
-        validations: [],
-        disabled: false,
-        omitted: false,
-        items: {
-          type: 'Link',
-          validations: [
-            {
-              linkContentType: [
-                'videoWithCaption',
-              ],
-            },
-          ],
-          linkType: 'Entry',
-        },
       },
     ],
-  }))
-  .then((contentType) => contentType.publish())
-  .then((contentType) => {
-    console.log(`${contentType.sys.id} is published`);
+    disabled: false,
+    omitted: false,
+    linkType: 'Asset',
+  });
 
-    // Add help text
-    client.getSpace(spaceId)
-      .then((space) => space.getEnvironment(envName))
-      .then((environment) => environment.getEditorInterfaceForContentType('videoSelector'))
-      .then((editorInterface) => {
-        editorInterface.controls[0].settings = { helpText: 'The web route of the selector' };
-        editorInterface.controls[1].settings = { helpText: 'Internal title. This is not displayed in the application.' };
-        editorInterface.controls[2].settings = { helpText: 'Title for the application to be displayed on the home screen. Example "STEM Stories"' };
-        editorInterface.controls[3].settings = { helpText: 'Optional. Use this field to add a background graphic to the video selection screen' };
-        editorInterface.controls[4].settings = { helpText: 'Width of the screen used in pixels. The default value of 1920 assumes a standard monitor in landscape mode.' };
-        editorInterface.controls[5].settings = { helpText: 'Height of the display screen used. The default of 1080 assumes a standard monitor in landscape mode.' };
-        editorInterface.controls[6].settings = { helpText: 'The videos included in your selector. These must be "Video with caption" entries in contentful.' };
-        return editorInterface.update();
-      })
-      .then((editorInterface) => console.log(`Editor interface ${editorInterface.sys.id} updated.`))
-      .catch(console.error);
+  videoSelector.createField('screenWidth', {
+    name: 'Screen width',
+    type: 'Integer',
+    localized: false,
+    required: true,
+    validations: [],
+    disabled: false,
+    omitted: false,
+    defaultValue: {
+      'en-US': 1920,
+    },
+  });
 
-    // IN PROGRESS - create empty entry
-    // client.getSpace(spaceId)
-    //   .then((space) => space.getEnvironment(envName))
-    //   .then((environment) => environment.createEntry('videoSelector', {
-    //     fields: {
-    //       slug: { 'en-US': 'example' },
-    //       title: { 'en-US': 'My Awesome Example' },
-    //       displayTitle: { 'en-US': 'My Awesome Example' },
-    //       screenWidth: { 'en-US': 1920 },
-    //       screenHeight: { 'en-US': 1080 },
-    //       selections: { 'en-US': [] },
-    //     },
-    //   }))
-    // .then((entry) => console.log(entry));
-  })
-  .catch(console.error);
+  videoSelector.changeFieldControl('screenWidth', 'builtin', 'numberEditor', {
+    helpText: 'Width of the screen in pixels. Default landscape value is 1920.',
+  });
+
+  videoSelector.createField('screenHeight', {
+    name: 'Screen height',
+    type: 'Integer',
+    localized: false,
+    required: true,
+    validations: [],
+    disabled: false,
+    omitted: false,
+    defaultValue: {
+      'en-US': 1080,
+    },
+  });
+
+  videoSelector.changeFieldControl('screenHeight', 'builtin', 'numberEditor', {
+    helpText: 'Width of the screen in pixels. Default landscape value is 1080.',
+  });
+
+  videoSelector.createField('selections', {
+    name: 'Selections',
+    type: 'Array',
+    localized: false,
+    required: true,
+    validations: [],
+    disabled: false,
+    omitted: false,
+    items: {
+      type: 'Link',
+      validations: [
+        {
+          linkContentType: [
+            'videoWithCaption',
+          ],
+        },
+      ],
+      linkType: 'Entry',
+    },
+  });
+};
