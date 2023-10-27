7469f4211074c1d566782a2efa25742372217f9f
Trygve Nordberg <tnordberg@smm.org>
2023-10-27 14:59:30 -0500

    feat: query for all video selectors on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { graphql, useStaticQuery, Link } from 'gatsby';
 
 function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulVideoSelector {
+          nodes {
+            slug
+            titleDisplay
+          }
+      }
+    }
+  `);
+
+  const { allContentfulVideoSelector } = data;
+
   return (
     <>
-      Go to route of your selector
+      <h1>Video selectors</h1>
+      <ul>
+        {allContentfulVideoSelector.nodes.map((node) => (
+          <li key={node.slug}>
+            <strong>{node.slug}</strong>
+            {' - '}
+            <Link to={node.slug}>
+              {node.titleDisplay}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
