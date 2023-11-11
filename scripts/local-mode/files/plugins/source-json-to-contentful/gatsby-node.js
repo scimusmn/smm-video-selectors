7b6e49b980f4cbdc380a8daefd1fb45b5c64ed39
Trygve Nordberg <tnordberg@smm.org>
2023-11-10 18:18:31 -0600

    feat: nested locale content in json structure

--- scripts/local-mode/files/plugins/source-json-to-contentful/gatsby-node.js
+++ scripts/local-mode/files/plugins/source-json-to-contentful/gatsby-node.js
@@ -22,50 +22,79 @@ exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => 
     actions.createNode(node);
   });
 
+  // Get default locale code
+  const defaultLocale = jsonData.locales.find((locale) => locale.default).code;
+  console.log('defaultLocale--->', defaultLocale);
+
+  // TODO: Replicate the below process for each locale, not just en-US
+  // When available, take content using the locale key, otherwise use the default
+  // Use the locale code to create a node ID
+
+  function getLocalized(fieldValue, localeCode) {
+    // Return the value as-is if it's not a locale object
+    if (typeof fieldValue !== 'object') {
+      return fieldValue;
+    }
+    // If it's an object with locale keys, return the value for requested locale
+    if (fieldValue[localeCode]) {
+      return fieldValue[localeCode];
+    }
+    // If the requested locale doesn't exist, fall back to default locale value
+    if (fieldValue[defaultLocale]) {
+      return fieldValue[defaultLocale];
+    }
+    console.warn(`Unable to localize field value ${localeCode} - ${fieldValue}`);
+    return null;
+  }
+
   // Transform Video Selectors from JSON into Contentful structure
   jsonData.videoSelectors.forEach((selector) => {
-    const transformedData = {
-      slug: selector.slug,
-      node_locale: 'en-US', // temporary
-      titleDisplay: selector.title,
-      screenWidth: selector.screenWidth,
-      screenHeight: selector.screenHeight,
-      backgroundGraphic: {
-        localFile: {
-          publicURL: selector.backgroundAsset,
-        },
-      },
-      selections: selector.selections.map((selection) => ({
-        titleDisplay: selection.title,
-        caption: {
-          title: selection.caption,
+    // Create one node per locale (to match Contentful's locale structure)
+    // These locale nodes are merged in front-end queries
+    jsonData.locales.forEach((locale) => {
+      const transformedData = {
+        slug: selector.slug,
+        node_locale: locale.code,
+        titleDisplay: getLocalized(selector.title, locale.code),
+        screenWidth: selector.screenWidth,
+        screenHeight: selector.screenHeight,
+        backgroundGraphic: {
           localFile: {
-            publicURL: selection.caption,
+            publicURL: selector.backgroundAsset,
           },
         },
-        media: {
-          localFile: {
-            publicURL: selection.media,
+        selections: selector.selections.map((selection) => ({
+          titleDisplay: getLocalized(selection.title, locale.code),
+          caption: {
+            title: 'title-does-this-even-matter',
+            localFile: {
+              publicURL: getLocalized(selection.caption, locale.code),
+            },
           },
-        },
-        selectionImage: {
-          localFile: {
-            publicURL: selection.selectionImage,
+          media: {
+            localFile: {
+              publicURL: selection.media,
+            },
           },
-        },
-      })),
-    };
+          selectionImage: {
+            localFile: {
+              publicURL: selection.selectionImage,
+            },
+          },
+        })),
+      };
 
-    const node = {
-      ...transformedData,
-      // Required fields
-      id: createNodeId(transformedData.slug),
-      internal: {
-        type: 'ContentfulVideoSelector',
-        contentDigest: createContentDigest(transformedData),
-      },
-    };
+      const node = {
+        ...transformedData,
+        // Required fields
+        id: createNodeId(`${transformedData.slug}-${locale.code}`),
+        internal: {
+          type: 'ContentfulVideoSelector',
+          contentDigest: createContentDigest(transformedData),
+        },
+      };
 
-    actions.createNode(node);
+      actions.createNode(node);
+    });
   });
 };
